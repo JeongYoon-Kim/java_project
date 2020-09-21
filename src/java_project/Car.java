7c41284680ed7bc84bc30c09191493d1bd67dc68
DESKTOP-D7EA9L1\LG <agd489@naver.com>
2020-09-21 18:32:30 +0900

    create car class , student class

---
+++ src/java_project/Car.java
@@ -0,0 +1,19 @@
+package java_project;
+
+public class Car {
+	String price;
+	String name;
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
