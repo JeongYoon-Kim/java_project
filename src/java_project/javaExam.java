14681ff197ce4815bbb7b8c7bf618bae1ea6421a
DESKTOP-D7EA9L1\LG <agd489@naver.com>
2020-09-21 18:16:33 +0900

    modify 출력 문자열 변경

---
+++ src/java_project/javaExam.java
@@ -0,0 +1,10 @@
+package java_project;
+
+public class javaExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Hello, eclipse");
+	}
+
+}
